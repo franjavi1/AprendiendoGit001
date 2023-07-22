85fe24fe52f5455822d54899c1f31f6b0bbed3ce
javier <francisco Javier Stevenin>
2023-07-22 14:21:18 -0300

    Updated2

--- src/aprendiendogit/AprendiendoGit.java
+++ src/aprendiendogit/AprendiendoGit.java
@@ -15,7 +15,7 @@ public class AprendiendoGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo!!!");    
+        System.out.println("Hola Mundo2!!!");    
     }
     
 }
